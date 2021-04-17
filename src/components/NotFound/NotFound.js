eef7e0e730a7de0e18b0645b96a69504b595c12c
lenapronina <kaknetak@gmail.com>
2021-04-18 00:13:39 +0300

    refactor: update card hover, replace images with sprite

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './NotFound.css';
-import CatImage from '../../images/cat-back.svg';
 
 function NotFound({ header, description }){
   return(
     <div className="notfound">
       <h2 className="notfound__heading">{header}</h2>
       <p className="notfound__paragraph">{description}</p>
-      <img 
-        className="notfound__image"
-        src={CatImage} 
-        alt="котик"
-      />
+      <div className="notfound__image"></div>
     </div>
   )
 }
